ba7b921b9389ebb1d28dcf055bb4893a059faed7
Daniel Bonsu <danielbonsu@protonmail.com>
2022-11-30 06:15:43 -0500

    adding search functionality

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
-import './Header.scss'
-const Header = () => {
-  return (
-    <div>
-      Header
-    </div>
-  )
-}
+import React, { useEffect,useState } from "react";
+import InputGroup from "react-bootstrap/InputGroup";
+import Form from "react-bootstrap/Form";
+import { FaUserAlt } from "react-icons/fa";
+import "./Header.scss";
 
-export default Header
+const Header = () => {
+
+ const [searchText, setSearchText] =  useState('') 
+  return (
+    <div className="header">
+      <div className="content">
+        <h1 className="mb-4">PROGRAMMERS REPOS</h1>
+        <input
+          type="text"
+          className="formControl"
+          placeholder="Find A User"
+          value={searchText}
+          onChange={e => setSearchText(e.target.value)}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,12 +3,16 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import { FaGithub } from 'react-icons/fa';
 
 const NavbarComponent = () => {
   return (
     <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand href="#home">Programmers Repos</Navbar.Brand>
+        <Navbar.Brand href="#home">
+            <span className='mx-2'><FaGithub size={50}/></span>
+            <span>Programmers Repo</span>
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
